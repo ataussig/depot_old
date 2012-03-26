030b6927f0a44988d496e5b115a347cd7f0010f2
Alex Taussig <ataussig@gmail.com>
2012-03-25 22:44:42 -0400

    A Smarter Cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,13 @@
 class StoreController < ApplicationController
+  
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
+  end
+  
   def index
     @products = Product.all
+    @count = increment_count 
   end
 
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,18 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy # :dependent => destroy indicates that destroying the cart destroys the line_item's
+  
+  def add_product(product_id)
+    current_item = line_items.find_by_product_id(product_id)
+    if current_item
+      current_item.quantity += 1    # If the current_item exists, then up the quantity by 1
+    else
+      current_item = line_items.build(:product_id => product_id)    # If the current_item doesn't exist, then create it
+    end
+    current_item
+  end
+  
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
+  
 end
