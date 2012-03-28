857581db1ca81dc4c56f128e4e32326c192b4179
Alex Taussig <ataussig@gmail.com>
2012-03-27 20:59:28 -0400

    Add a dash of Ajax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
+# If the attribute in question is true, then set the CSS style to display:none for the block. Regardless, rap the result in a <div> tag.
+
 module ApplicationHelper
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"] = "display: none"
+    end
+    content_tag("div", attributes, &block)
+  end
 end
